f7f1a3974d705d9a175bc85ea52cb70d78a20606
feofilov.ilya <feofilov.ilya@gmail.com>
2019-04-29 16:12:12 +0300

    add greeting user

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
+import getNameSayHi from "..";
+
+console.log('Welcome to the Brain Games!\n');
+console.log(`Hello, ${getNameSayHi()}!`);
